fc88067a416a2632fba90aca741dd3f43ca813e7
Tallyta Triolo <tally.soares@gmail.com>
2022-06-16 00:09:48 -0400

    updating codes

--- script/main.js
+++ script/main.js
@@ -1,39 +1,20 @@
-// we always start with a module to encapsulate our own code
-// it is called an IIFE (Immediately - Invoked Function Expression)
-
 (() => {
-	// collect All of the elements that we want the user to interact with and also elements that to change
-	// JS holds these in memory so that it can access them later (these are eleementss in the HTML)
+
 	let theThumbnails = document.querySelectorAll("#buttonHolder img"),
+
 		gameBoard = document.querySelector(".puzzle-board"),
 		pzlPieces = document.querySelectorAll('.puzzle-pieces img'),
 		dropZones = document.querySelectorAll(".drop-zone");
 
-	/* the Thumbails collects all of the image elements into an array-like container that looks like this:
-	[
-		<img src="images/buttonZero.jpg" alt="thumbnail">
-		<img src="images/buttonOne.jpg" alt="thumbnail">
-		<img src="images/buttonTwo.jpg" alt="thumbnail">
-		<img src="images/buttonThree.jpg" alt="thumbnail">
-	]*/
-
-	// .gameBoard {
-	// 	background-image: URL("../images/backGround0.jps)")
-	// }
 
 	const imageNames = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
 
 	function changeImageSet() {
-		// the "this" keyword refers to the element that triggers this function (the nav button we click with the custom data attribute of bgref)
-		// debugger;
-		// update the background image thumb pieces
+		
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
 
-		let clickedThumb = this; //this is the elemnet (thumbnail) we clicked on
+		let clickedThumb = this; 
 
-		//debugger; // this will pause code execution on this line. like pushing pause on Netflix/Amazon Prime
-
-		// update the draggable piece's src attribute one at a time
 		pzlPieces.forEach((piece, index) => {
 			piece.src = `images/${imageNames[index] + clickedThumb.dataset.bgref}.jpg`;
 		});
@@ -42,12 +23,11 @@
 	function allowDrag(event) {
 		console.log("started draggin me");
 
-		//create a reference to the element we are dragging so we can retrieve it later.
 		event.dataTransfer.setData("draggedEl", this.id);
 	}
 
 	function allowDragOver(event) {
-		// override default behaviour on certain elements when an event happens
+		
 		event.preventDefault();
 		console.log("started draggin over me");
 	}
@@ -57,15 +37,11 @@
 		console.log("dropped on me");
 
 		let droppedElId = event.dataTransfer.getData("draggedEl");
-		// if this.childElementCount is greater than 0, dont execute anything else. Just exit the function.
-
-		//retrieve the dragged el by its ID, and then put it inside the current drop zone
+		
 		this.appendChild(document.querySelector(`#${droppedElId}`));
 	}
 
-	// how to we want the user to interact with the elements that we collected earlier?
-	// events are things like clicks, drags, double-clickss, keypresses... all the ways that a user can interact with a mouse, a keyboard etc.
-	// add event handling here => loopthrough theThumbnails array and add event handling to each image
+	
 	theThumbnails.forEach(item => item.addEventListener("click", changeImageSet));
 	pzlPieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
 	
