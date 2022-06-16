4db2018ef94ef158f8d9611462383d6424096fad
Tallyta Triolo <tally.soares@gmail.com>
2022-06-16 09:59:08 -0400

    first bug fixed

--- script/main.js
+++ script/main.js
@@ -5,6 +5,7 @@
 		gameBoard = document.querySelector(".puzzle-board"),
 		pzlPieces = document.querySelectorAll('.puzzle-pieces img'),
 		dropZones = document.querySelectorAll(".drop-zone");
+		// zonePieces = document.querySelector('.puzzle-pieces');
 
 
 	const imageNames = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
@@ -33,7 +34,15 @@
 	}
 
 	function allowDrop(event) {
+
 		event.preventDefault();
+
+		// 1º Bug - prevents two pieces from dropping in the same box //
+		if (this.children.length >= 1){
+
+			return;
+		}
+
 		console.log("dropped on me");
 
 		let droppedElId = event.dataTransfer.getData("draggedEl");
@@ -41,8 +50,16 @@
 		this.appendChild(document.querySelector(`#${droppedElId}`));
 	}
 
+	// function resetPuzzle(){
+	// 	for (let loop = 0; loop < puzzlePieces.length; loop = loop + 1){
+	// 		zonePieces.appendChild(puzzlePieces[loop]);
+	// 	}
+	// }
 	
-	theThumbnails.forEach(item => item.addEventListener("click", changeImageSet));
+	theThumbnails.forEach(item => 
+		item.addEventListener("click", changeImageSet));
+		// item.addEventListener('click', resetPuzzle)
+
 	pzlPieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
 	
 	//set up the drop zone event handling
@@ -51,4 +68,6 @@
 		zone.addEventListener("drop", allowDrop);
 	});
 	
+	// changeImageSet.call(theThumbnails[0]); // emulates a click on the first bottom button
+
 })();
