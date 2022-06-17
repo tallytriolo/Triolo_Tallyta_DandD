7cf839add3f1a71a18f12e756de1122e1d5618dc
Tallyta Triolo <tally.soares@gmail.com>
2022-06-17 01:08:48 -0400

    fixing puzzlePieces dropZone

--- script/main.js
+++ script/main.js
@@ -1,17 +1,26 @@
 (() => {
-
+	// collect All of the elements that we want the user to interact with and also elements that to change
+	// JS holds these in memory so that it can access them later (these are eleementss in the HTML)
 	let theThumbnails = document.querySelectorAll("#buttonHolder img"),
 
 		gameBoard = document.querySelector(".puzzle-board"),
 		pzlPieces = document.querySelectorAll('.puzzle-pieces img'),
 		dropZones = document.querySelectorAll(".drop-zone");
-		// zonePieces = document.querySelector('.puzzle-pieces');
 
+	/* the Thumbails collects all of the image elements into an array-like container that looks like this:
+	[
+		<img src="images/buttonZero.jpg" alt="thumbnail">
+		<img src="images/buttonOne.jpg" alt="thumbnail">
+		<img src="images/buttonTwo.jpg" alt="thumbnail">
+		<img src="images/buttonThree.jpg" alt="thumbnail">
+	]*/
 
 	const imageNames = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
 
 	function changeImageSet() {
-		
+		// the "this" keyword refers to the element that triggers this function (the nav button we click with the custom data attribute of bgref)
+		// debugger;
+		// update the background image thumb pieces
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
 
 		let clickedThumb = this; 
@@ -34,28 +43,21 @@
 	}
 
 	function allowDrop(event) {
-
 		event.preventDefault();
-
-		// 1º Bug - prevents two pieces from dropping in the same box //
-		if (this.children.length >= 1){
-
-			return;
-		}
-
 		console.log("dropped on me");
 
 		let droppedElId = event.dataTransfer.getData("draggedEl");
-		
+
+		// 1º Bug - prevents two pieces from dropping in the same box //
+		// if this.childElementCount is greater than 0, dont execute anything else. Just exit/return the function.
+		if (this.childElementCount > 0){
+			return;
+		}
+
+		//retrieve the dragged el by its ID, and then put it inside the current drop zone
 		this.appendChild(document.querySelector(`#${droppedElId}`));
 	}
 
-	// function resetPuzzle(){
-	// 	for (let loop = 0; loop < puzzlePieces.length; loop = loop + 1){
-	// 		zonePieces.appendChild(puzzlePieces[loop]);
-	// 	}
-	// }
-	
 	theThumbnails.forEach(item => 
 		item.addEventListener("click", changeImageSet));
 		// item.addEventListener('click', resetPuzzle)
@@ -68,6 +70,5 @@
 		zone.addEventListener("drop", allowDrop);
 	});
 	
-	// changeImageSet.call(theThumbnails[0]); // emulates a click on the first bottom button
 
 })();
